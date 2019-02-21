31370b61cb7d8a01e6d29a81427be5f98d250a57
Luke Nadur <19835357+intricate@users.noreply.github.com>
2019-02-20 23:18:30 -0600

    [CBR-482] WIP: Wrap GState-related getTip* functions

--- db/src/Pos/DB/BlockIndex.hs
+++ db/src/Pos/DB/BlockIndex.hs
@@ -12,12 +12,17 @@ import           Universum
 import qualified Database.RocksDB as Rocks
 
 import           Pos.Binary.Class (serialize')
-import           Pos.Chain.Block (BlockHeader (..), HeaderHash, headerHash)
+import           Pos.Chain.Block (BlockHeader (..), HeaderHash,
+                     genHeaderPrevBlock, headerHash)
+import           Pos.Chain.Update (ConsensusEra (..))
 import           Pos.DB.Class (DBTag (BlockIndexDB), MonadBlockDBRead,
                      MonadDB (..))
+import           Pos.DB.Error (DBError (DBMalformed))
 import           Pos.DB.Functions (dbGetBi)
 import           Pos.DB.GState.Tip (getTipSomething)
-import           Pos.DB.Update.GState.BlockVersion (blockIndexKey)
+import           Pos.DB.Update.GState.BlockVersion (blockIndexKey,
+                     getConsensusEra)
+import           Pos.Util.Util (maybeThrow)
 
 -- | Returns header of block that was requested from Block DB.
 getHeader
@@ -27,7 +32,20 @@ getHeader = dbGetBi BlockIndexDB . blockIndexKey
 
 -- | Get 'BlockHeader' corresponding to tip.
 getTipHeader :: MonadBlockDBRead m => m BlockHeader
-getTipHeader = getTipSomething "header" getHeader
+getTipHeader = do
+    era <- getConsensusEra
+    tipHeader <- getTipHeaderFromDb
+    case era of
+        Original -> pure tipHeader
+        OBFT _   -> case tipHeader of
+            BlockHeaderMain _      -> pure tipHeader
+            BlockHeaderGenesis gbh -> do
+                let prevBlockHash :: HeaderHash
+                    prevBlockHash = gbh ^. genHeaderPrevBlock
+                maybeThrow (DBMalformed $ "sup") =<< getHeader prevBlockHash
+  where
+    getTipHeaderFromDb :: MonadBlockDBRead m => m BlockHeader
+    getTipHeaderFromDb = getTipSomething "header" getHeader
 
 -- | Writes batch of headers into the block index db.
 putHeadersIndex :: (MonadDB m) => [BlockHeader] -> m ()
--- db/src/Pos/DB/GState/Tip.hs
+++ db/src/Pos/DB/GState/Tip.hs
@@ -15,11 +15,18 @@ import           Universum
 
 import           Formatting (sformat, stext, (%))
 
-import           Pos.Chain.Block (HeaderHash)
-import           Pos.DB.Class (MonadDB, MonadDBRead (..))
+import           Pos.Chain.Block (BlockHeader (..), HeaderHash,
+                     genHeaderPrevBlock)
+import           Pos.Chain.Update (ConsensusEra (..))
+import           Pos.DB.Class (DBTag (..), MonadBlockDBRead, MonadDB,
+                     MonadDBRead (..))
 import           Pos.DB.Error (DBError (DBMalformed))
+import           Pos.DB.Functions (dbGetBi)
 import           Pos.DB.GState.Common (gsGetBi, gsPutBi)
+import           Pos.DB.Update.GState.BlockVersion (blockIndexKey,
+                     getConsensusEra)
 import           Pos.Util.Util (maybeThrow)
+
 
 ----------------------------------------------------------------------------
 -- Getters
@@ -27,7 +34,29 @@ import           Pos.Util.Util (maybeThrow)
 
 -- | Get current tip from GState DB.
 getTip :: MonadDBRead m => m HeaderHash
-getTip = maybeThrow (DBMalformed "no tip in GState DB") =<< getTipMaybe
+getTip = do
+    era <- getConsensusEra
+    tipHash <- getTipFromDb
+    tipHeader <- maybe (error "ok") pure =<< getHeader tipHash
+    case era of
+        Original -> pure tipHash
+        OBFT _   -> case tipHeader of
+            BlockHeaderMain _      -> pure tipHash
+            BlockHeaderGenesis gbh -> do
+                let prevBlockHash :: HeaderHash
+                    prevBlockHash = gbh ^. genHeaderPrevBlock
+                prevBlockHeader <- getHeader prevBlockHash
+                case prevBlockHeader of
+                    Nothing -> throwM (DBMalformed $ "sup")
+                    Just _  -> pure prevBlockHash
+  where
+    getTipFromDb :: MonadDBRead m => m HeaderHash
+    getTipFromDb = maybeThrow (DBMalformed "no tip in GState DB") =<< getTipMaybe
+    --
+    getHeader
+        :: (MonadBlockDBRead m)
+        => HeaderHash -> m (Maybe BlockHeader)
+    getHeader = dbGetBi BlockIndexDB . blockIndexKey
 
 getTipSomething
     :: forall m smth.
